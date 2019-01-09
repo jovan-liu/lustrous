2baedb114b9b652d46e2eea86ac18f4f61cbec4d
jovan.liu <jovanliu.it@gmail.com>
2019-01-09 17:35:55 +0800

    update pom.xml add maven compiler plugin

--- lustrous-test/src/main/java/io/jovan/liu/lustrous/test/string/StringTest.java
+++ lustrous-test/src/main/java/io/jovan/liu/lustrous/test/string/StringTest.java
@@ -1,4 +1,9 @@
 package io.jovan.liu.lustrous.test.string;
 
 public class StringTest {
+    public static void main(String[] args) {
+        System.out.println("Aa".hashCode());
+
+        System.out.println("BB".hashCode());
+    }
 }
